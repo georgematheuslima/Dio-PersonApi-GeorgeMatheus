8490a1762a0bde2c4cd3407bb66192b9fd258f96
George <georgematheuslima@outlook.com>
2021-07-13 18:14:06 -0300

    Added more structure files

---
+++ src/test/java/one/digitalInovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,45 @@
+package one.digitalInovation.personapi.service;
+
+
+import one.digitalInovation.personapi.dto.MessageResponseDTO;
+import one.digitalInovation.personapi.dto.request.PersonDTO;
+import one.digitalInovation.personapi.entity.Person;
+import one.digitalInovation.personapi.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, succesMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
---
+++ src/test/java/one/digitalInovation/personapi/utils/PhoneUtils.java
@@ -0,0 +1,27 @@
+package one.digitalInovation.personapi.utils;
+
+import one.digitalInovation.personapi.dto.request.PhoneDTO;
+import one.digitalInovation.personapi.entity.Phone;
+import one.digitalInovation.personapi.enums.PhoneType;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
